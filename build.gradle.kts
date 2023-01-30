96c8086948d9e831265b2f3af3750f2c12f9493e
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-01-30 23:02:29 +0200

    Add build.gradle.kts in root
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories { google(); mavenCentral() }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Versions.GRADLE}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.NAVIGATION}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT}")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
