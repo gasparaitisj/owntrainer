0c5d518fcc609569ddbc288e1b7972aad389cd7a
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-02-06 19:18:14 +0200

    Implement Bottom Navigation in MainScreen.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/java/com/gasparaiciukas/owntrainer/ui/main/MainScreen.kt
+++ app/src/main/java/com/gasparaiciukas/owntrainer/ui/main/MainScreen.kt
@@ -1,9 +1,13 @@
 package com.gasparaiciukas.owntrainer.ui.main
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.AutoGraph
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.RestaurantMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -17,24 +21,38 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.gasparaiciukas.owntrainer.ui.home.HomeScreen
+import com.gasparaiciukas.owntrainer.ui.meals.MealsScreen
+import com.gasparaiciukas.owntrainer.ui.progress.ProgressScreen
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf("Songs", "Artists", "Playlists")
+    val items = listOf(
+        BottomNavigationItem(
+            label = "Home",
+            icon = Icons.Filled.Home
+        ),
+        BottomNavigationItem(
+            label = "Meals",
+            icon = Icons.Filled.RestaurantMenu
+        ),
+        BottomNavigationItem(
+            label = "Progress",
+            icon = Icons.Filled.AutoGraph
+        )
+    )
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Progress Screen") }
-            )
-        },
+        topBar = { TopAppBar(title = { Text(items[selectedItem].label) }) },
         bottomBar = {
             NavigationBar {
                 items.forEachIndexed { index, item ->
                     NavigationBarItem(
-                        icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                        label = { Text(item) },
+                        icon = { Icon(item.icon, item.label) },
+                        label = { Text(item.label) },
                         selected = selectedItem == index,
                         onClick = { selectedItem = index }
                     )
@@ -43,9 +61,20 @@ fun MainScreen() {
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
-            Text(text = "Hello")
+            when (selectedItem) {
+                0 -> HomeScreen()
+                1 -> MealsScreen()
+                2 -> ProgressScreen()
+            }
         }
     }
 }
+
+data class BottomNavigationItem(
+    val label: String,
+    val icon: ImageVector
+)
