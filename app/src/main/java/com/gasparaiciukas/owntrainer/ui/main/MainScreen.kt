2c0508a80d05a91c9fb19b34b74e7a37d819d8b0
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-02-06 18:55:17 +0200

    Add MainScreen.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ app/src/main/java/com/gasparaiciukas/owntrainer/ui/main/MainScreen.kt
@@ -0,0 +1,51 @@
+package com.gasparaiciukas.owntrainer.ui.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Progress Screen") }
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            Text(text = "Hello")
+        }
+    }
+}
