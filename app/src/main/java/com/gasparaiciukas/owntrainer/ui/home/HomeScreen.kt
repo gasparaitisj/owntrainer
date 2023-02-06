9ccff821f0d2e4ff649ab2b4da9d89a916626ea5
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-02-06 19:17:25 +0200

    Add HomeScreen.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ app/src/main/java/com/gasparaiciukas/owntrainer/ui/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.gasparaiciukas.owntrainer.ui.home
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Text(text = "Home")
+}
