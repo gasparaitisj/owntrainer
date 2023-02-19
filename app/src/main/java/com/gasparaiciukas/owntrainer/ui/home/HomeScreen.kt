06058ffa8469ea017128cd6a6fc5f43724c544b7
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-02-19 06:24:19 +0200

    Add `HomeNavigation` in HomeScreen.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/java/com/gasparaiciukas/owntrainer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gasparaiciukas/owntrainer/ui/home/HomeScreen.kt
@@ -1,13 +1,67 @@
 package com.gasparaiciukas.owntrainer.ui.home
 
-import android.annotation.SuppressLint
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Text(text = "Home")
+    Column {
+        HomeNavigation()
+    }
+}
+
+@Composable
+fun HomeNavigation() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton(
+            onClick = {
+                // Do nothing.
+            }
+        ) {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                imageVector = Icons.Filled.ArrowBackIos,
+                contentDescription = "Back arrow"
+            )
+        }
+        Text(
+            text = "Sample Text",
+            style = MaterialTheme.typography.headlineLarge
+        )
+        IconButton(
+            onClick = {
+                // Do nothing.
+            }
+        ) {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                imageVector = Icons.Filled.ArrowForwardIos,
+                contentDescription = "Forward arrow"
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HomeNavigationPreview() {
+    HomeNavigation()
 }
